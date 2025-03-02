d3913092a4d7b29c2ea72a82476f88ca10672270
Loaf0 <ninjakin102@outlook.com>
2025-03-01 23:07:45 -0500

    Fixed Pokemon Descriptions

--- src/PAASFunctions/StatsLibTester.java
+++ src/PAASFunctions/StatsLibTester.java
@@ -1,21 +1,23 @@
 package PAASFunctions;
 
+import java.util.Arrays;
+
 public class StatsLibTester {
     public static void main(String[] args) {
 
         StatsLibrary functions = new StatsLibrary();
 
-        double[] doubleValues = {.904,.929,.901};
+        double[] doubleValues = {.904, .929, .901};
 
         System.out.println(functions.geometricDistribution(.2, 3));
-//        System.out.println(functions.binomialDistribution(10, 4, .15));
-//        System.out.println("Standard Deviation : " + functions.standardDeviation(doubleValues));
-//        System.out.println("              Mean : " + functions.getMean(doubleValues));
-//        System.out.println("              Mode : " + Arrays.toString(functions.getMode(doubleValues)));
-//        System.out.println("            Median : " + functions.getMedian(doubleValues));
-//        System.out.println("         Factorial : " + functions.bigFactorial(6));
-//        System.out.println("      Permutations : " + functions.permutation(8, 3));
-//        System.out.println("      Combinations : " + functions.combination(9, 5));
+        System.out.println(functions.binomialDistribution(10, 4, .15));
+        System.out.println("Standard Deviation : " + functions.standardDeviation(doubleValues));
+        System.out.println("              Mean : " + functions.getMean(doubleValues));
+        System.out.println("              Mode : " + Arrays.toString(functions.getMode(doubleValues)));
+        System.out.println("            Median : " + functions.getMedian(doubleValues));
+        System.out.println("         Factorial : " + functions.bigFactorial(6));
+        System.out.println("      Permutations : " + functions.permutation(8, 3));
+        System.out.println("      Combinations : " + functions.combination(9, 5));
 
     }
 }
