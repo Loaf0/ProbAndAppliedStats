adb86de8984c5d186007673aa5445d1675a086c1
Loaf0 <ninjakin102@outlook.com>
2025-03-09 21:36:57 -0400

    Added Poker And Blackjack Simulations

--- src/Cards/Simulator.java
+++ src/Cards/Simulator.java
@@ -7,21 +7,21 @@ import java.io.PrintWriter;
 public class Simulator {
 
     public static void runBlackJack(){
-        String path = "../DataExports/mulliganBrickExport.csv";
+        String path = "../DataExports/blackjackExport.csv";
         File f = new File(path);
 
         f.getParentFile().mkdirs();
 
         try (PrintWriter pw = new PrintWriter(f)){
             StringBuilder sb = new StringBuilder();
-            sb.append("Draw Until,").append("Losses\n");
+            sb.append("Draw Until,").append("Losses out of 10000 turns\n");
 
             int losses = 0;
 
             for (int drawLimit = 10; drawLimit <= 21; drawLimit++){
                 losses = 0;
                 for (int i = 0; i < 10000; i++) {
-                    if(new Blackjack().playTurn(drawLimit))
+                    if(!new Blackjack().playTurn(drawLimit))
                         losses++;
                 }
                 sb.append(drawLimit).append(",").append(losses).append("\n");
